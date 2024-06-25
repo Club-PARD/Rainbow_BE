728cf046993788cfb01e079366d7e42919f97fdd
Junhyung Lee <joseph3.lee@gmail.com>
2024-06-25 13:05:05 +0900

    add sampling

---
+++ src/main/java/com/example/rainbow_be/RainbowBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rainbow_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainbowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainbowBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rainbow_be/RainbowBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rainbow_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainbowBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
