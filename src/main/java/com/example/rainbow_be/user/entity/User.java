d46942f23780104d1b6f87a202af23b1427b7057
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-06-26 13:42:31 +0900

    :white_check_mark:  Create user entity Id

--- src/main/java/com/example/rainbow_be/user/entity/User.java
+++ src/main/java/com/example/rainbow_be/user/entity/User.java
@@ -1,4 +1,22 @@
 package com.example.rainbow_be.user.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
 }
