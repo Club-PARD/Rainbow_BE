1749c3d2de10fd8e7c116ef80224ac59ee5b0a96
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-07-04 17:18:05 +0900

    :ambulance: fix the Google Oauth2 login

--- src/main/java/com/pard/rainbow_be/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/pard/rainbow_be/oauth/PrincipalOauth2UserService.java
@@ -23,24 +23,22 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest)
             throws OAuth2AuthenticationException {
-        log.info("📍 google userRequest: "+oAuth2UserRequest );
+        log.info("📍 google userRequest: " + oAuth2UserRequest);
 
         OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
-        log.info("📍 oauth : "+ oAuth2User.getAttributes());
+        log.info("📍 oauth: " + oAuth2User.getAttributes());
 
-        //// 객체에서 다른 정보(클라이언트 ID, 클라이언트 시크릿 등)를 가져오는 데 사용하지만, 여기선 사용 X
-        // String registrationId = oAuth2UserRequest.getClientRegistration().getRegistrationId();
-
-        //// userNameAttributeName : 사용자의 고유 식별자 속성 이름, 업체마다 이게 다름.
-        // ex) Google -> sub , Facebook -> id
         String userNameAttributeName = oAuth2UserRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
         OAuthAttributes attributes = OAuthAttributes.of(userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
         httpSession.setAttribute("user", new SessionUser(user));
 
-        return super.loadUser(oAuth2UserRequest);
+        log.info("📍 session user: " + new SessionUser(user));
+
+        return oAuth2User;
     }
+
     private User saveOrUpdate(OAuthAttributes attributes) {
         User user = userRepo.findByEmail(attributes.getEmail())
                 .map(entity -> {
--- src/main/java/com/pard/rainbow_be/oauth/controller/AuthController.java
+++ src/main/java/com/pard/rainbow_be/oauth/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.pard.rainbow_be.oauth.controller;
 
 import com.pard.rainbow_be.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -14,17 +16,20 @@ public class AuthController {
     private UserService userService;
 
     @GetMapping("/loginForm")
-    public String login(){
+    public String loginForm(){
+        log.info("로그인 되었엉");
         return "loginForm";
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     @ResponseBody
     public ResponseEntity<Boolean> login(@RequestParam String email, @RequestParam String password) {
         boolean isAuthenticated = userService.validateUser(email, password);
         if (isAuthenticated) {
+            log.info("로그인 성공: " + email);
             return ResponseEntity.ok(true);
         } else {
+            log.info("로그인 실패: " + email);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
         }
     }
