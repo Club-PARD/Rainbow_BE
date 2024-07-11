05b4990949fa32daf8106a4ee1558d15910f9697
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-07-12 02:33:03 +0900

    🔊 add log s3 controller

--- src/main/java/com/pard/rainbow_be/picture/controller/S3Controller.java
+++ src/main/java/com/pard/rainbow_be/picture/controller/S3Controller.java
@@ -3,6 +3,7 @@ package com.pard.rainbow_be.picture.controller;
 import com.pard.rainbow_be.picture.service.S3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class S3Controller {
@@ -19,7 +21,7 @@ public class S3Controller {
     @PostMapping("/file/profile")
     @Operation(summary = "사진 업로드", description = "사진을 올리시변 변형시켜 url 값만 드립니다ㅎ")
     public String uploadProfileImage(@RequestParam("image") MultipartFile multipartFile) throws IOException {
-
+        log.info("\uD83D\uDCCD upload picture");
         return s3Service.uploadProfile(multipartFile);
     }
 }
