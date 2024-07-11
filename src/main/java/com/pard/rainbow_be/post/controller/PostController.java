3de084234ccc1f75fd680b0dad28dce1b99b3bbe
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-07-11 14:20:08 +0900

    🔇 remove log in post

--- src/main/java/com/pard/rainbow_be/post/controller/PostController.java
+++ src/main/java/com/pard/rainbow_be/post/controller/PostController.java
@@ -30,35 +30,30 @@ public class PostController {
     @Operation(summary = "게시물 등록", description = "해당 유저가 게시물을 만드는 메서드")
     public String createPost(@RequestBody PostCreateDTO postCreateDTO, @PathVariable UUID ownerId){
         postService.createPost(postCreateDTO, ownerId);
-        log.info("📍make Post");
         return "Post Create Success";
     }
 
     @GetMapping("/post/find/{ownerId}")
     @Operation(summary = "해당 유저의 게시물 전체 보기", description = "유저 아이디를 통해 해당 유저의 전체 게시물을 보게 만드는 메서드")
     public List<PostReadDTO> readAll(@PathVariable UUID ownerId){
-        log.info("📍view all Post for User");
         return postService.readAll(ownerId);
     }
 
     @GetMapping("/post/find/{ownerId}/{postId}")
     @Operation(summary = "postId 별로 게시물 보기", description = "게시물을 보게 하는 메서드")
     public PostReadDTO readPost(@PathVariable UUID ownerId, @PathVariable Long postId){
-        log.info("📍view Post");
         return postService.findById(ownerId, postId);
     }
 
     @GetMapping("/post/count/{ownerId}")
     @Operation(summary = "userId 를 이용해서 해당 유저의 전체 게시물 수 리턴", description = "게시물 갯수 세기 메서드")
     public Integer countByUserId(@PathVariable UUID ownerId){
-        log.info("📍 count Posts ");
         return postService.countByUserId(ownerId);
     }
 
     @GetMapping("/post/community")
     @Operation(summary = "Community 사진 보여주가", description = "해당 유저가 게시물을 만드는 메서드")
     public List<CommunityReadDto> readsTheLatestPost(){
-        log.info("📍 커뮤니티 란 나왔당");
         return postService.readsTheLatestPost();
     }
 
@@ -66,7 +61,6 @@ public class PostController {
     @Operation(summary = "postId 를 이용해서 게시물 수정", description = "게시물을 수정 메서드")
     public String updateByPid(@PathVariable Long postId, @RequestBody PostUpdateDTO postUpdateDTO){
         postService.updateById(postId, postUpdateDTO);
-        log.info("📍update Post");
         return "Update Success";
     }
 
@@ -74,7 +68,6 @@ public class PostController {
     @DeleteMapping("/post/{postId}")
     public String deleteById(@PathVariable Long postId){
         postService.deleteById(postId);
-        log.info("📍delete Post");
         return "Delete Success";
     }
 
